daf78a53c206430a80fccdc163bb675c9137536f
Linaz <zellik122711@mail.ru>
2023-11-04 20:33:35 +0800

    Third Commit

--- src/MainSwing.java
+++ src/MainSwing.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 public class MainSwing {
     public static void main(String[] args) {
@@ -7,6 +9,17 @@ public class MainSwing {
 //        jFrame.addMouseListener(new SimpleClickListener());
         jFrame.setVisible(true);
         SimpleClickListener mouseListener = new SimpleClickListener();
-        jFrame.addMouseListener(mouseListener);
+//        jFrame.addMouseListener(mouseListener);
+        jFrame.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                System.out.println(e.getX() + " " + e.getY());
+            }
+        });
     }
 }
